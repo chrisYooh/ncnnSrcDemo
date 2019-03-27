9b87d43c95e69ba36d4f530403edb19594c93d7b
杨一凡 <340019109@qq.com>
2019-03-27 14:45:30 +0800

    修正步骤描述

--- NcnnSrcDemo/NcnnSrcDemo/ViewController.mm
+++ NcnnSrcDemo/NcnnSrcDemo/ViewController.mm
@@ -31,34 +31,34 @@
     NSString *paramPath = [[NSBundle mainBundle] pathForResource:@"squeezenet_v1.1" ofType:@"param"];
     NSString *binPath = [[NSBundle mainBundle] pathForResource:@"squeezenet_v1.1" ofType:@"bin"];
     
-    /* Step1 : 加载.parma 文件 */
+    /* Step1.1 : 加载.parma 文件 */
     ncnn_net.load_param(paramPath.UTF8String);
     
-    /* Step2 : 加载.bin 文件 */
+    /* Step1.2 : 加载.bin 文件 */
     ncnn_net.load_model(binPath.UTF8String);
 }
 
 - (void)detect {
     
-    /* Step3 : 构建并配置 提取器 */
+    /* Step2.1 : 构建并配置 提取器 */
     ncnn::Extractor extractor = ncnn_net.create_extractor();
     extractor.set_light_mode(true);
 
-    /* Step4 : 设置输入（将图片转换成ncnn::Mat结构作为输入） */
+    /* Step2.2 : 设置输入（将图片转换成ncnn::Mat结构作为输入） */
     UIImage *srcImage = [UIImage imageNamed:@"mouth"];
     ncnn::Mat mat_src;
     [self __fillNcnnMat:mat_src withImage:srcImage];
     extractor.input("data", mat_src);
     
-    /* Step5 : 提取输出 */
+    /* Step2.3 : 提取输出 */
     ncnn::Mat mat_dst;
     extractor.extract("prob", mat_dst);
     
-    /* Step6 : 结果处理(获取检测概率最高的5种物品，认为存在) */
+    /* Step3.1 : 结果处理(获取检测概率最高的5种物品，认为存在) */
     NSArray *rstArray = [self __arrayFromMat:mat_dst];
     NSArray *top5Array = [self __top5ArrayFromArray:rstArray];
     
-    /* Step7 : 打印输出 */
+    /* Step3.2 : 打印输出 */
     NSLog(@"%@", top5Array);
     
     /* 说明：该Demo中发现输出的第一项是 index 为 673 的项目，
